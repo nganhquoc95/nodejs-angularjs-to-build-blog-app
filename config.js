d7087e80175d4953f1a6c3a5a9060c83b573a810
Anh Quoc <nganhquoc95@gmail.com>
2017-08-18 14:31:01 +0700

    Fix link cors

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	host_cors: "http://localhost:4200",
+	host_cors: "http://no-accent.96.lt",
+	// host_cors: "http://localhost:4200",
 	port: "8000",
 	db_host: "localhost",
 	db_name: "blogapps"
